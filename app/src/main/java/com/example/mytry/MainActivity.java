65fa95bcef77dc17f5ac76a84fbe0f8aa5b823be
wanghuijuan <1499695895@qq.com>
2019-03-19 21:28:06 +0800

    默认

---
+++ app/src/main/java/com/example/mytry/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.mytry;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Log.d("data", "onCreateexecute");
+        Button button1=(Button) findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(MainActivity.this,"You clicked Button 1", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
